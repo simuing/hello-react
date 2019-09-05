ee53300a1793191b116aa57e2f623239697281cd
simuing <wdd0988@daum.net>
2019-09-06 01:47:39 +0900

    add component

---
+++ src/Calendar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return {
+            <div>
+                new project!123
+            </div>
+        }
+    }
+}
