92af9ff36639466351a5985127a59e1a6578708f
simuing <wdd0988@daum.net>
2020-10-21 00:38:01 +0900

    13장 리덕스로 리엑트 애플리케이션 상태 관리(Provider, reducers, types, redux 세팅)

---
+++ react-study-projects/redux-counter/src/action/ActionType.js
@@ -0,0 +1,8 @@
+/* Acrion 종류들을 선언합니다.
+   앞에 export를 붙이면 나중에
+   import * as types from './ActionTypes'를 할 수 있습니다.
+*/
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
--- react-study-projects/redux-counter/src/index.js
+++ react-study-projects/redux-counter/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+// 리덕스 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux'; //손쉽게 스토어를 연동할 수 있도록 도와주는 컴포넌트
+
+// 스토어 설정
+const store = createStore(reducers);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +25,13 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/* 참고 tip
+<React.StrictMode> 기능
+   안전하지 않은 생명주기를 사용하는 컴포넌트 발견
+   레거시 문자열 ref 사용에 대한 경고
+   권장되지 않는 findDOMNode 사용에 대한 경고
+   예상치 못한 부작용 검사
+   레거시 context API 검사
+*/
