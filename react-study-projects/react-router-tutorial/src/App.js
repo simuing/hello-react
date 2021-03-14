dd543dd82f76f9accfe491c6363664b5ed365c97
simuing <wdd0988@daum.net>
2021-03-14 22:41:50 +0900

    16장 리액트 라우터 - query-string 활용

--- react-study-projects/react-router-tutorial/src/App.js
+++ react-study-projects/react-router-tutorial/src/App.js
@@ -8,14 +8,7 @@ import {
 const App = () => {
   return (
     <div>
-      {/* <Route exact path="/" component={Home}/> */}
       <Route path="/" component={Home}/>
-      
-      {/* 파라미터를 받는 첫 번째 방법 - exact로 구분하여 두줄 작성 */}
-      {/* <Route exact path="/about" component={About}/>
-      <Route path="/about/:name" component={About}/> */}
-
-      {/* 파라미터를 받는 두 번째 방법 - 물음표를 추가하여 한줄로 작성 */}
       <Route path="/about/:name?" component={About}/>
     </div>
   )
