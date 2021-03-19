1db263fbd7f6c97609c3f81f982d5d2a88da45bf
simuing <wdd0988@daum.net>
2021-03-19 23:27:17 +0900

    20.6.2 블로그 프로젝트 - 에디터 상태 관리 추가 (store 인식불가 에러해결 필요)

--- react-study-projects/blog/blog-frontend/src/components/editor/EditorHeader/EditorHeader.js
+++ react-study-projects/blog/blog-frontend/src/components/editor/EditorHeader/EditorHeader.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './EditorHeader.scss';
 import classNames from 'classnames/bind';
+import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
-const EditorHeader = () => (
-    <div className={cx('post-list')}>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
+const EditorHeader = (onGoBack, onSubmit) => (
+    <div className={cx('editor-header')}>
+        <div className={cx('black')}>
+            <Button onClick={onGoBack} theme="outline">뒤로가기</Button>
+        </div>
+        <div className={cx('submit')}>
+            <Button onClick={onSubmit} theme="outline">작성하기</Button>
+        </div>
     </div>
 );
 
--- react-study-projects/blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
+++ react-study-projects/blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
@@ -9,7 +9,7 @@ const PostInfo = () => (
         <div className={cx('info')}>
             <h1>타이틀</h1>
             <div className={cx('tags')}>
-                <a>#태그</a> <a>#태그</a> <a>#태그</a>
+                <a href="#">#태그</a> <a href="#">#태그</a> <a href="#">#태그</a>
             </div>
             <div className={cx('date')}>Oct 29, 2017</div>
         </div>
