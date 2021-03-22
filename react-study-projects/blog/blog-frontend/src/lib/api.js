5df3745fd2882ac1e592456ce8a8aec39e550b39
simuing <wdd0988@daum.net>
2021-03-23 00:01:50 +0900

    21.5.7 푸터, 로그인 모달 연동

--- react-study-projects/blog/blog-frontend/src/lib/api.js
+++ react-study-projects/blog/blog-frontend/src/lib/api.js
@@ -6,3 +6,7 @@ export const getPost = (id) => axios.post(`/api/posts/${id}`);
 export const getPostList = ({tag, page}) => axios.get(`/api/posts/?${queryString.stringify({tag, page})}`);
 export const editPost = ({id, title, body, tags}) => axios.get(`/api/posts/${id}`, {title, body, tags});
 export const removePost = (id) => axios.delete(`/api/posts/${id}`);
+
+export const login = (password) => axios.post('/api/auth/login', { password });
+export const checkLogin = () => axios.get('/get/auth/check');
+export const logout = () => axios.post('/api/auth/logout');
--- react-study-projects/blog/blog-frontend/src/store/modules/base.js
+++ react-study-projects/blog/blog-frontend/src/store/modules/base.js
@@ -7,10 +7,21 @@ import { pender } from 'redux-pender';
 const SHOW_MODAL = 'base/SHOW_MODAL';
 const HIDE_MODAL = 'base/HIDE_MODAL';
 
+const LOGIN = 'base/LOGIN';
+const LOGOUT = 'base/LOGOUT';
+const CHECK_LOGIN = 'base/CHECK_LOGIN';
+const CHANGE_PASSWORD_INPUT = 'base/CHANGE_PASSWORD_INPUT';
+const INITIALIZE_LOGIN_MODAL = 'base/INITIALIZE_LOGIN_MODAL';
 
 // action creators
 export const showModal = createAction(SHOW_MODAL);
 export const hideModal = createAction(HIDE_MODAL);
+
+export const login = createAction(LOGIN, api.login);
+export const logout = createAction(LOGOUT, api.logout);
+export const checkLogin = createAction(CHECK_LOGIN, api.checkLogin);
+export const changePasswordInput = createAction(CHANGE_PASSWORD_INPUT, api.changePasswordInput);
+export const initializeLoginModal = createAction(INITIALIZE_LOGIN_MODAL, api.initializeLoginModal);
 
 //initial state
 const initialState = Map({
@@ -18,7 +29,14 @@ const initialState = Map({
     modal: Map({
         remove: false,
         login: false // 추후 구현될 로그인 모달
-    })
+    }),
+
+    //로그인 모달 상태
+    loginModal: Map({
+        passwork: '',
+        error: false
+    }),
+    logged: false // 현재 로그인 상태
 });
 
 // reducer
@@ -30,5 +48,30 @@ export default handleActions({
     [HIDE_MODAL]: (state, action) => {
         const { payload: modalName } = action;
         return state.setIn(['modal', modalName], false)
-    }
+    },
+    ...pender({
+        type: LOGIN,
+        onSuccess: (state, action) => {
+            return state.set('logged', true)
+        },
+        onError: (state, action) => {
+            return stete.setIn(['loginModal', 'error'], true)
+                        .setIn(['loginModal', 'password'], '')
+        }
+    }),
+    ...pender({
+        type: CHECK_LOGIN,
+        onSuccess: (state, action) => {
+            const { logged } = action.payload.data;
+            return state.set('logged', logged)
+        }
+    }),
+    [CHANGE_PASSWORD_INPUT]: (state, action) => {
+        const { payload: value } = action;
+        return stete.setIn('loginModal', initialState.get('loginModal'));
+    },
+    [INITIALIZE_LOGIN_MODAL]: (state, action) => {
+        // 로그인 모달의 상태를 초기 상태로 설정(텍스트/오류 초기화)
+        return stete.setIn('loginModal', initialState.get('loginModal'));
+    },
 }, initialState)
