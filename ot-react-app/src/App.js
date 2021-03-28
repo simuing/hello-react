b8cf128128aefa26931b9f92e7437a91ddc42960
simuing <wdd0988@daum.net>
2021-03-29 00:19:45 +0900

    생활코딩 React - 7. delete 구현 (완료) [project done]

--- ot-react-app/src/App.js
+++ ot-react-app/src/App.js
@@ -114,9 +114,27 @@ class App extends Component {
           data={this.state.contents}
         ></TOC>
         <Control onChangeMode={(_mode)=>{
-          this.setState({
-            mode: _mode
-          })
+          if(_mode === 'delete') {
+            if(window.confirm('really delete?')){
+              let _contents = Array.from(this.state.contents);
+              let i = 0;
+              while(i < this.state.contents.length){
+                if(_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                  this.setState({
+                    mode: 'welcome',
+                    contents: _contents
+                  })
+                  break;
+                }
+                i = i + 1;
+              }
+            }
+          } else {
+            this.setState({
+              mode: _mode
+            })
+          }
         }}></Control>
         {this.getContent()}
       </div>
