d98821fdab1e058dde618b9348ff478686423667
simuing <wdd0988@daum.net>
2021-03-29 02:28:41 +0900

    React class vs function style - 4.2.2. 함수에서 라이프 사이클 구현 하기 - useEffect (완료)

--- react-class-vs-function/src/App.js
+++ react-class-vs-function/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -13,10 +13,12 @@ function App() {
 
 /* 함수형 컴포넌트 */
 // props(인자)로 데이터를 받는다.
-// 간단한 표현식에 사용했었지만, hook에 의해 다양한 표현식에 사용이 가능해졌다.
-// React.useState 로 state를 사용할 수 있다.
+// 간단한 표현식에 사용했었지만, hook(useState, useEffect,...)에 의해 다양한 표현식에 사용이 가능해졌다.
+// React.useState로 state를 사용할 수 있다.
+// React.useEffect로 
+let funcStyle = 'color:yellow';
+let funcId = 0;
 function FuncComp(props) {
-
   // (1) 인자로 출력하는 방식
   // let number = props.initNumber;
 
@@ -26,23 +28,27 @@ function FuncComp(props) {
   let number = numberState[0];    //state
   let setNumber = numberState[1]; //setState
 
-  let dateState = useState((new Date()).toString());
-  let _date = dateState[0];
-  let setDate = dateState[1];
-
-  // 간단하게 작성하는 방법
-  // let [_date, setDate] = useState((new Date()).toString());
-
-
-  console.log('numberState', numberState);
-
+  // 간단하게 선언된 date state 정보
+  let [_date, setDate] = useState((new Date()).toString());
+  
+  //side effect
+  useEffect(()=>{
+    console.log('%cclass => useEffect'+(++funcId), funcStyle);
+    document.title = number + ' : ' + _date; //useEffect 예시
+  });
+  
+  // debugger;
+  console.log('%cclass => render'+(++funcId), funcStyle);
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number : {number}</p>
       <p>Data : {_date}</p>
       <input type="button" value="random" onClick={()=>{
-        setNumber(Math.floor(Math.random()*10))
+        setNumber(Math.random())
+      }}></input>
+      <input type="button" value="date" onClick={()=>{
+        setDate((new Date()).toString())
       }}></input>
     </div>
   )
@@ -57,7 +63,25 @@ class ClassComp extends Component {
     number: this.props.initNumber,
     date: (new Date()).toString()
   }
+
+  // componentWillMount는 리액트17부터 지원되지 않으며, 생성자로 대체하기를 권장한다.
+  // componentWillMount() {
+  //   console.log('%cclass => componentWillMount', 'color:red');
+  // }
+
+  // shouldComponentUpdate는 컴포넌트에 변화가 생길 경우 실행되는 함수이다.
+  // true로 return할 경우 render()가 실행된다.
+  shouldComponentUpdate() {
+    console.log('%cclass => shouldComponentUpdate', 'color:red');
+    return true;
+  }
+
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', 'color:red');
+  }
+
   render() {
+    console.log('%cclass => render', 'color:red');
     return (
       <div className="container">
         <h2>class style component</h2>
@@ -65,7 +89,12 @@ class ClassComp extends Component {
         <p>Data : {this.state.date}</p>
         <input type="button" value="random" onClick={()=>{
           this.setState({
-            number: Math.floor(Math.random()*10)
+            number: Math.random()
+          })
+        }}></input>
+        <input type="button" value="date" onClick={()=>{
+          this.setState({
+            date: (new Date()).toString()
           })
         }}></input>
       </div>
