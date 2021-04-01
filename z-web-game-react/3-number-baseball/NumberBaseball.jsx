ab4e02b50cdffe1f8ed4a9228ef63f26053d2b88
simuing <wdd0988@daum.net>
2021-04-01 20:41:20 +0900

    웹 게임을 만들며 배우는 React 3-8 숫자야구 Hooks로 전환하기 (Try 전환 완료)

--- z-web-game-react/3-number-baseball/NumberBaseball.jsx
+++ z-web-game-react/3-number-baseball/NumberBaseball.jsx
@@ -26,17 +26,21 @@ class NumberBaseball extends Component {
     }
 
     onSubmitForm = (e) => {
+        const { value, tries, answer} = this.state;
+
          e.preventDefault();
-         if(this.state.value === this.state.answer.join('')) {
-            this.setState({
-                result: '홈런!',
-                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+         if(value === answer.join('')) {
+            this.setState((prevState) => {
+                return {
+                    result: '홈런!',
+                    tries: [...prevState.tries, { try: value, result: '홈런!' }],
+                }
             });
          } else {
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length >= 9) {
+            if (tries.length >= 9) {
                 this.setState({
                     result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
                 });
@@ -48,22 +52,23 @@ class NumberBaseball extends Component {
                 });
             } else {
                 for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball += 1;
                     }
                 }
-                this.setState({
-                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
-                    value: '',
+                this.setState((prevState) => {
+                    return {
+                        tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                        value: '',
+                    }
                 })
             }
          }
     }
 
     onChangeinput = (e) => {
-        console.log(this.state.answer);
         this.setState({
             value: e.target.value, 
         })
