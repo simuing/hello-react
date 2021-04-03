18c5afdd4cf8856cc36117dd039cf4a2dbaae9bf
simuing <wdd0988@daum.net>
2021-04-03 16:16:07 +0900

    7-5. 테이블 최적화하기 (done)

--- z-web-game-react/7-tic-tac-toe/Td.jsx
+++ z-web-game-react/7-tic-tac-toe/Td.jsx
@@ -1,7 +1,19 @@
-import React, { useCallback, memo } from 'react';
+import React, { useCallback, memo, useEffect, useRef } from 'react';
 import { CLICK_CELL } from './TicTacToe';
 
-const Td = memo(({ dispatch, rowIndex, cellIndex, cellData }) => {
+const Td = memo(({ rowIndex, cellIndex, dispatch, cellData }) => {
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(
+            rowIndex === ref.current[0],
+            cellIndex === ref.current[1],
+            dispatch === ref.current[2],
+            cellData === ref.current[3],
+        );
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+    }, [rowIndex, cellIndex, dispatch, cellData])
+
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
         if (cellData) {
@@ -9,8 +21,6 @@ const Td = memo(({ dispatch, rowIndex, cellIndex, cellData }) => {
         }
         dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
     },[cellData]);
-
-    
 
     return (
         <td onClick={onClickTd}>{cellData}</td>
--- z-web-game-react/7-tic-tac-toe/Tr.jsx
+++ z-web-game-react/7-tic-tac-toe/Tr.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Td from './Td';
 
-const Tr = ({ dispatch, rowData, rowIndex}) => {
+const Tr = memo(({ dispatch, rowData, rowIndex}) => {
     return (
         <tr>
             {Array(rowData.length).fill().map((td, i) => (
                 <Td dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>
+
+                //useMemo 적용 예시
+                // useMemo(
+                //     () => <Td dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>,
+                //     [rowData[i]],
+                // )
             ))}
         </tr>
     )
-}
+});
 
 export default Tr;
